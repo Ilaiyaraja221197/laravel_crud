5d415bb66295ceeb6bdd0e2010bfb6f619ac815e
Ilaiyaraja <ilaiyaraja221197@gmail.com>
2024-03-06 23:45:15 +0530

    product update

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('products',\App\Http\Controllers\ProductController::class);
+//Route::get('/', function () {
+//    return view('welcome');
+//});
